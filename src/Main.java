2c80b0476bff5aeb89ee71cf7be6a8773c154aaa
Vaibhav Diwan <vaibhav.diwan@niit.com>
2022-08-19 14:47:55 +0530

    Student array demo

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.util.Objects;
+import java.util.Scanner;
 
 public class Main {
     private int id;
@@ -17,7 +18,8 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        Demo obj = new Demo();
+        new Main().toString();
+        /*Demo obj = new Demo();
 
         obj.setId(10);
         obj.setName("Vaibhav");
@@ -34,7 +36,71 @@ public class Main {
         System.out.println("obj.equals(main) = " + obj.equals(main));
 
         System.out.println("demo = " + demo);
-        System.out.println("obj = " + obj);
+        System.out.println("obj = " + obj);*/
+        // array of student object
+        //type[] arrayName = new type[int size];
+        Student[] students = new Student[5];
+        Student s1;
+        Scanner sc = new Scanner(System.in);
+        for (int i = 0; i < students.length; i++) {
+            //initialize all the index by calling constructor on each index
+            s1 = new Student();
+            System.out.println("enter name");
+            s1.setName(sc.nextLine());
+            System.out.println("enter email");
+            s1.setEmail(sc.nextLine());
+            System.out.println("enter roll number");
+            s1.setRollNo(sc.nextInt());
+            sc.nextLine();
+            students[i] = s1;
+            /*students[i].setName("Vaibhav");
+            students[i].setEmail("abhb@example.com");
+            students[i].setMobile(1234567888L);*/
+            System.out.println("students[i] = " + students[i]);
+
+        }
+        students[4].setName("Akash");
+        for (int i = 0; i < students.length; i++) {
+            System.out.println(students[i]);
+           /* System.out.println("" + students[i].getClass().getName()+"@" +
+                    Integer.toHexString(students[i].hashCode()));*/
+        }
+
+        // sorting as per name or roll number
+        // searching for a student -> by values, name or roll number or email,
+        // compare(equality) object if I implemented equals
+    }
+    public boolean search(Student searchObject, Student[] studentList){
+        boolean response = false;
+        for (int i = 0; i < studentList.length; i++) {
+            if(searchObject.equals(studentList[i])){
+                response = true;
+                break;
+            }
+        }
+
+        return response;
+    }
+    public Student[] sorting(Student[] listOfStudent){
+        //sorting on roll number
+        for (int i = 0; i < listOfStudent.length; i++) {
+            for (int j = 0; j < listOfStudent.length - 1; j++) {
+                if(listOfStudent[j].getRollNo() > listOfStudent[j+1].getRollNo()){
+                    //swap of objects
+                    /*Student temp = listOfStudent[j];
+                    listOfStudent[j] = listOfStudent[j+1];
+                    listOfStudent[j+1] = temp;*/
+                    swap(j, j+1,listOfStudent);
+                }
+            }
+        }
+        return listOfStudent;
+    }
+
+    private void swap(int index, int nextIndex, Student[] arrayOfStudent){
+        Student temp = arrayOfStudent[index];
+        arrayOfStudent[index] = arrayOfStudent[nextIndex];
+        arrayOfStudent[nextIndex] = temp;
     }
 
 }
--- src/Student.java
+++ src/Student.java
@@ -3,7 +3,7 @@ import java.util.Objects;
 public class Student {
     private String name;
     private String email;
-    private long mobile;
+    private int rollNo;
 
     public String getName() {
         return name;
@@ -21,33 +21,51 @@ public class Student {
         this.email = email;
     }
 
-    public long getMobile() {
-        return mobile;
+    public int getRollNo() {
+        return rollNo;
     }
 
-    public void setMobile(long mobile) {
-        this.mobile = mobile;
+    public void setRollNo(int rollNo) {
+        this.rollNo = rollNo;
     }
 
     public String toString(){
-        return name + ", " + email + ", " + mobile;
+        return name + ", " + email + ", " + rollNo;
     }
 
-    @Override
-    public boolean equals(Object obj) {
-        if(this == obj){
-            return true;
-        }
-        if(obj instanceof Student){
-            Student  student = (Student) obj;
-            if(this.name.equals(student.name) && this.email.equals(student.email)
-                    && this.mobile == student.mobile){
+    /*@Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Student student)) return false;
+
+        if (getMobile() != student.getMobile()) return false;
+        if (getName() != null ? !getName().equals(student.getName()) : student.getName() != null) return false;
+        return getEmail() != null ? getEmail().equals(student.getEmail()) : student.getEmail() == null;
+    }*/
+
+    /*  @Override
+        public boolean equals(Object obj) {
+            if(this == obj){
                 return true;
             }
-        }
-        return false;
+            if(obj instanceof Student){
+                Student  student = (Student) obj;
+                if(this.name.equals(student.name) && this.email.equals(student.email)
+                        && this.mobile == student.mobile){
+                    return true;
+                }
+            }
+            return false;
+        }*/
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return rollNo == student.rollNo && Objects.equals(name, student.name) && Objects.equals(email, student.email);
     }
-    public int hashCode(){
-        return Objects.hash(name, email, mobile);
-    }
+
+
 }
